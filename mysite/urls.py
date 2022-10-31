5cb44052c42dbf4d557fecb172bc30fbad7d6e02
alextowe <alextowe96@gmail.com>
2022-10-31 18:53:28 -0400

    created app for static pages

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,10 @@
 
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+	path('', include('pages.urls')),
+	path('blog', include('blog.urls')),
 	path('admin/', admin.site.urls),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def blog(request):
-	return HttpResponse("Blog Page")
+def index(request):
+	return HttpResponse("Home Page")
