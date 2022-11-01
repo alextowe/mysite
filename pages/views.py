fc5a6715aac1ba08d9b6f8788406f5e2893a1614
alextowe <alextowe96@gmail.com>
2022-11-01 18:54:59 -0400

    simple navbar

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Home Page")
+	context = {
+
+	}
+
+	return render(request, 'pages/index.html', context)
