2fe274ba5a4e5e1a5f352a8164052ecb77c3b085
alextowe <alextowe96@gmail.com>
2022-11-17 20:03:07 -0500

    added template for blog page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pages.models import Logo, Page
+from pages.views import navbar
 
 def blog(request):
-	return HttpResponse("Blog Page")
+	logo, pages = navbar(Logo, Page)
+	
+	context = {
+		'logo': logo,
+		'pages': pages,
+	}
+	
+	return render(request, 'blog/blog.html', context)
