e03540a5149cdad957b00fb6d5ef181b7d54c2bd
alextowe <alextowe96@gmail.com>
2022-11-17 20:18:46 -0500

    added blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'author', 'author_img', 'date_added', 'is_published')
+	list_display_links = ('id', 'title')
+	search_field = ('title')
+	list_per_page = 25
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=50)
+	author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+	author_img = models.ImageField(blank=True, upload_to='photos/logo/%Y/%m/%d/')
+	content = models.TextField(blank=True)
+	date_added = models.DateTimeField(auto_now= True)
+	is_published = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return self.title
