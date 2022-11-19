8da5c9ca62a00bcae14f4d30fb65a4bdda6f25d9
alextowe <alextowe96@gmail.com>
2022-11-19 18:24:56 -0500

    fixed tabbing issue

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,13 +8,3 @@ def render_cards():
 	posts = Post.objects.order_by('-date_added').filter(is_published=True)
 	context = {'posts':posts}
 	return context
-
-@register.inclusion_tag('blog/card.html')
-def render_post(slug):
-	"""Renders post from slug."""
-	posts = Post.objects.filter(is_published=True)
-	for post in posts:
-		if post.slug == slug:
-			current_post = post
-	context = {'post':current_post,}
-	return context
