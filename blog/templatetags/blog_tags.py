d0bc6b3580ddeb6c7f2eb627adbada398f49c817
alextowe <alextowe96@gmail.com>
2022-11-19 19:20:13 -0500

    width for blog cards

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,7 +2,7 @@ from django import template
 from blog.models import Post
 register = template.Library()
 
-@register.inclusion_tag('blog/card.html')
+@register.inclusion_tag('blog/cards.html')
 def render_cards():
 	"""Renders blog posts in order of newest to oldest."""
 	posts = Post.objects.order_by('-date_added').filter(is_published=True)
